442c0577a94367901775bf4f23f29d641e715ee5
Stoyko Kolev <stoyko.kolev@scalefocus.com>
2021-06-30 13:28:07 +0300

    [SPFA-10] Create CategoryItems Scene UI

---
+++ SFPortfolioApp/SFPortfolioApp/Modules/HomeModule/CategoryItemsScene/CategoryItemTableViewCell/CategoryItemTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CategoryItemTableViewCell.swift
+//  SFPortfolioApp
+//
+//  Created by Stoyko Kolev on 30.06.21.
+//
+
+import UIKit
+
+class CategoryItemTableViewCell: UITableViewCell {
+    
+    // MARK: - IBOutlets
+    @IBOutlet private weak var titleLabel: UILabel!
+    
+}
