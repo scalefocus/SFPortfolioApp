b207d9115a44b6f63a77c51c482df9199a8fbf65
Stoyko Kolev <stoyko.kolev@scalefocus.com>
2021-07-02 10:00:11 +0300

    [no-issue] Add Category Coordinator

---
+++ SFPortfolioApp/SFPortfolioApp/Modules/CategoryModule/CategoryCoordinator.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryCoordinator.swift
+//  SFPortfolioApp
+//
+//  Created by Stoyko Kolev on 2.07.21.
+//
+
+import SFBaseKit
+
+class CategoryCoordinator: Coordinator {
+    
+    // MARK: - Properties
+    private let navigationController: UINavigationController
+    
+    // MARK: - Initializers
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+}
