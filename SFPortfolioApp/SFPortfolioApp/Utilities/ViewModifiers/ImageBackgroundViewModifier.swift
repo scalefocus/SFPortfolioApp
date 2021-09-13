bbd873cad16a2f00fd2f015847f672f269f10a7f
Stoyko Kolev <stoyko.kolev@scalefocus.com>
2021-09-13 10:56:54 +0300

    [SFPA-52] Fix transition when pushing UIHostingController with SwiftUI content

--- SFPortfolioApp/SFPortfolioApp/Utilities/ViewModifiers/ImageBackgroundViewModifier.swift
+++ SFPortfolioApp/SFPortfolioApp/Utilities/ViewModifiers/ImageBackgroundViewModifier.swift
@@ -10,17 +10,37 @@ import SwiftUI
 /// A view modifier used for attaching an image background to a specific view.
 struct ImageBackgroundViewModifier: ViewModifier {
     
+    // MARK: - Properties
     let image: Image
+    let logo: Image
+    private let logoWidth = Constants.General.logoWidth
+    private let logoHeight = Constants.General.logoHeight
     
-    private var backgroundView: some View {
-        image
-            .resizable()
-            .edgesIgnoringSafeArea(.all)
-    }
-    
+    // MARK: - Main View
     func body(content: Content) -> some View {
         content
             .background(backgroundView)
+            .edgesIgnoringSafeArea(.all)
+    }
+    
+    // MARK: - Private Views
+    private var footerView: some View {
+        HStack {
+            logo
+                .resizable()
+                .frame(width: logoWidth, height: logoHeight, alignment: .center)
+            Spacer()
+        }
+        .background(Color(.barBackground))
+    }
+    
+    private var backgroundView: some View {
+        VStack(spacing: .zero) {
+            image
+                .resizable()
+            footerView
+        }
+        .edgesIgnoringSafeArea(.all)
     }
     
 }
