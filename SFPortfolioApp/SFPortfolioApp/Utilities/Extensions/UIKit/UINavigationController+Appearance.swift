665d20d808bb0b39e1f9901bbb3944d08943ff88
Stoyko Kolev <stoyko.kolev@scalefocus.com>
2021-09-13 10:56:54 +0300

    [SFPA-52] Fix transition when pushing UIHostingController with SwiftUI content

--- SFPortfolioApp/SFPortfolioApp/Utilities/Extensions/UIKit/UINavigationController+Appearance.swift
+++ SFPortfolioApp/SFPortfolioApp/Utilities/Extensions/UIKit/UINavigationController+Appearance.swift
@@ -11,12 +11,19 @@ extension UINavigationController {
     
     /// Configure navigation bar tint and title appearance.
     func setupNavigationBarAppearance() {
-        navigationBar.tintColor = .barTint
-        navigationBar.backgroundColor = .barBackground
+        let appearance = UINavigationBarAppearance()
+        appearance.shadowColor = .clear
+        appearance.shadowImage = UIImage()
+        appearance.backgroundColor = .barBackground
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.barTint]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.barTint]
+        
+        navigationBar.standardAppearance = appearance
+        navigationBar.compactAppearance = appearance
+        navigationBar.scrollEdgeAppearance = appearance
         navigationBar.prefersLargeTitles = true
-        navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.barTint]
-        navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.barTint]
-        navigationBar.blurred()
+        navigationBar.tintColor = .barTint
+        navigationBar.isTranslucent = true
     }
     
 }
