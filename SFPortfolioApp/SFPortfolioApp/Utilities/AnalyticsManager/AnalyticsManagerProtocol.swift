ac2923125a54de5a3a2f5e34823cd439e837e5b9
Stoyko Kolev <stoyko.kolev@scalefocus.com>
2021-10-14 09:28:48 +0300

    [SFPA-62] Integrate AppCenter Analytics and Crashes

---
+++ SFPortfolioApp/SFPortfolioApp/Utilities/AnalyticsManager/AnalyticsManagerProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  AnalyticsManagerProtocol.swift
+//  SFPortfolioApp
+//
+//  Created by Stoyko Kolev on 18.10.21.
+//
+
+import Foundation
+
+protocol AnalyticsManagerProtocol {
+    
+    /// Configures the app to send analytics to the service.
+    func start()
+    
+}
